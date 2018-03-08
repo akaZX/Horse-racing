ba4fe56f77c0e33f0a795d4b70081c25d662f334
akaZX <34091448+akaZX@users.noreply.github.com>
2018-03-08 13:55:36 +0000

    working elements

--- game.js
+++ game.js
@@ -1,35 +1,56 @@
 
+var interval = 0;
+var element;
+var positionLeft = 0;
+var positionTop = 0;
 
 
-//controll buttons
-function keyPressed(event){          
-           
-    if(event.keyCode == 37){
-        left = true;
+//horses movement
+function moveRight(){    
+
+    var startButton = document.querySelector('#start');
+    startButton.removeEventListener('click', start);
+    startButton.style.color = "Red";
+    startButton.style.cursor = "not-allowed";
+    
+     var horse = document.getElementsByClassName('horse')
+    
+    
+    for (var i = 0; i < horse.length; i++) {
+        var randomSpeed = Math.floor(Math.random()*3);        
+        positionTop = horse[i].offsetTop;
+		positionLeft = horse[i].offsetLeft;       
+        
+      
+        if(positionTop > innerHeight * 0.65){
+            horse[i].className = "horse runRight"; 
+            horse[i].style.left = positionLeft + randomSpeed +'px'; }       
+        
+        
+
+                    
+            
+           }       
+    
+}
+
+    
+
+
+
+function start(){          
+        interval = setInterval(moveRight, 10);
+        
+       
     }
-    if(event.keyCode == 38){
-        up = true;
-    }
-    if (event.keyCode == 39){
-        right = true;
-    }
-    if(event.keyCode == 40){
-        down = true;
-    }
+   
+
+    
+
+
+function myLoadEvent() {
+    var startButton = document.querySelector('div #start');
+    startButton.addEventListener('click', start);
    }
 
-   function keyReleased(event){
-       if(event.keyCode == 37){
-       left = false;
-       }
-
-       if(event.keyCode == 38){
-        up = false;
-        }
-        if (event.keyCode == 39){
-            right = false;
-        }
-        if (event.keyCode == 40){
-            down = false;
-        }
-   }
+   document.addEventListener('DOMContentLoaded', myLoadEvent);
