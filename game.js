b71f168c7f7c3dd425e2865fe893e847a4829ebf
akaZX <34091448+akaZX@users.noreply.github.com>
2018-03-08 22:38:34 +0000

    bega ratu

--- game.js
+++ game.js
@@ -4,6 +4,15 @@ var element;
 var positionLeft = 0;
 var positionTop = 0;
 
+var nearFinish = false;
+
+
+var randomTurn = [];
+randomTurn[0] = 0.07;
+randomTurn[1] = 0.05;
+randomTurn[2] = 0.02;
+randomTurn[3] = 0.00;
+
 
 //horses movement
 function moveRight(){    
@@ -21,25 +30,52 @@ function moveRight(){
         positionTop = horse[i].offsetTop;
 		positionLeft = horse[i].offsetLeft;       
         
-      
-        if(positionTop > innerHeight * 0.65){
+        //moves horses right
+        if(nearFinish == false && positionTop > innerHeight * (0.74 - randomTurn[i]) && positionLeft < innerWidth*(0.74 + randomTurn[i]) && positionLeft > innerWidth*0.05){
             horse[i].className = "horse runRight"; 
-            horse[i].style.left = positionLeft + randomSpeed +'px'; }       
-        
-        
+            horse[i].style.left = positionLeft + randomSpeed  +'px';         
+        }  
 
-                    
+        //moves horses up          
+        else if ( nearFinish == false && positionTop > innerHeight *(0.07 + randomTurn[i]) && positionLeft >= innerWidth*(0.74 + randomTurn[i]) ){
+            horse[i].className = "horse runUp"; 
+            horse[i].style.top = positionTop - randomSpeed +'px'; }
+
+        //moves horses left
+        if( nearFinish == false && positionTop < innerHeight*(0.07 + randomTurn[i]) &&  positionLeft >= innerWidth*(0.12-randomTurn[i])){
+            horse[i].className = "horse runLeft"; 
+            horse[i].style.left = positionLeft - randomSpeed +'px'; }
+
+        //moves horse down   
+        else if (positionLeft <= innerWidth*(0.16-randomTurn[i]) && positionTop < innerHeight * (0.74 - randomTurn[i]) ){
+            horse[i].className = "horse runDown"; 
+            horse[i].style.top = positionTop + randomSpeed +'px'; 
+            //nearFinish = true;
+        }
+
+        /*else if( nearFinish == true && positionTop > innerHeight * (0.74 - randomTurn[i]) && positionLeft < innerWidth*(0.74 + randomTurn[i]) && positionLeft > innerWidth*0.05){
+                      
+            horse[i].className = "horse runRight"; 
+            horse[i].style.left = positionLeft + randomSpeed  +'px'; 
+
+            var finishLine = document.querySelector('#startline');
+            var finish = finishLine.offsetLeft + 70;
             
-           }       
-    
-}
+            if (positionLeft == finish ){
+                horse[i].style.left = finish;
+                horse[i].className = "horse standRight";
+            }*/
+        }
+                    
+                
+        }
 
     
 
 
 
 function start(){          
-        interval = setInterval(moveRight, 10);
+        interval = setInterval(moveRight, 1);
         
        
     }
