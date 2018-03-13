752c1b76a51243aacf5a96d1c3d32df418158e84
akaZX <34091448+akaZX@users.noreply.github.com>
2018-03-13 01:50:45 +0000

    latest commit

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
-
-var scoreBoard =[];
+var scoreBoard = [];
 var interval = 0;
 var element;
 var positionLeft = 0;
@@ -24,93 +23,106 @@ turningCorner1[3] = 0.00;
 
 
 //horses movement
-function moveRight(){    
+function moveRight() {
 
     var startButton = document.querySelector('#start');
     startButton.removeEventListener('click', start);
     startButton.style.color = "Red";
     startButton.style.cursor = "not-allowed";
-    
-     var horse = document.getElementsByClassName('horse')
-    
-    
+
+    var horse = document.getElementsByClassName('horse')
+
+
     for (var i = 0; i < horse.length; i++) {
-        var randomSpeed = Math.floor(Math.random()*7);        
+        var randomSpeed = Math.floor(Math.random() * 7);
         positionTop = horse[i].offsetTop;
-		positionLeft = horse[i].offsetLeft;       
-        
+        positionLeft = horse[i].offsetLeft;
+
         //moves horses right
-        if(positionTop > innerHeight * (0.72 - turningCorner1[i]) && positionLeft < innerWidth*(0.75 + turningCorner1[i]) && positionLeft > innerWidth*0.05){
-            horse[i].className = "horse runRight"; 
-            horse[i].style.left = positionLeft + randomSpeed  +'px';         
-        }  
+        if (positionTop > innerHeight * (0.72 - turningCorner1[i]) && positionLeft < innerWidth * (0.75 + turningCorner1[i]) && positionLeft > innerWidth * 0.05) {
+            horse[i].className = "horse runRight";
+            horse[i].style.left = positionLeft + randomSpeed + 'px';
+        }
 
         //moves horses up          
-        else if ( positionTop > innerHeight *(0.07 + turningCorner1[3-i]) && positionLeft >= innerWidth*(0.75 + turningCorner1[i]) ){
-            horse[i].className = "horse runUp"; 
-            horse[i].style.top = positionTop - randomSpeed +'px'; }
+        else if (positionTop > innerHeight * (0.07 + turningCorner1[3 - i]) && positionLeft >= innerWidth * (0.75 + turningCorner1[i])) {
+            horse[i].className = "horse runUp";
+            horse[i].style.top = positionTop - randomSpeed + 'px';
+        }
 
         //moves horses left
-        if(positionTop < innerHeight*(0.07 + turningCorner1[3-i]) &&  positionLeft >= innerWidth*(0.12-turningCorner1[3-i])){
-            horse[i].className = "horse runLeft"; 
-            horse[i].style.left = positionLeft - randomSpeed +'px'; }
+        if (positionTop < innerHeight * (0.07 + turningCorner1[3 - i]) && positionLeft >= innerWidth * (0.12 - turningCorner1[3 - i])) {
+            horse[i].className = "horse runLeft";
+            horse[i].style.left = positionLeft - randomSpeed + 'px';
+        }
 
         //moves horse down   
-        else if ( positionLeft <= innerWidth*(0.14-turningCorner1[3-i]) && positionTop < innerHeight * (0.74 - turningCorner1[i]) ){
-            horse[i].className = "horse runDown"; 
-            horse[i].style.top = positionTop + randomSpeed +'px'; 
-            nearFinish=true;
+        else if (positionLeft <= innerWidth * (0.14 - turningCorner1[3 - i]) && positionTop < innerHeight * (0.74 - turningCorner1[i])) {
+            horse[i].className = "horse runDown";
+            horse[i].style.top = positionTop + randomSpeed + 'px';
+            nearFinish = true;
         }
         //final movement to right
-        else if( nearFinish == true && positionTop > innerHeight * (0.72 - turningCorner1[i]) && positionLeft < innerWidth*(0.75 + turningCorner1[i]) && positionLeft > innerWidth*0.05){
-            nearFinish = true;         
-            horse[i].className = "horse runRight"; 
-            horse[i].style.left = positionLeft + randomSpeed  +'px'; 
+        else if (nearFinish == true && positionTop > innerHeight * (0.72 - turningCorner1[i]) && positionLeft < innerWidth * (0.75 + turningCorner1[i]) && positionLeft > innerWidth * 0.05) {
+            nearFinish = true;
+            horse[i].className = "horse runRight";
+            horse[i].style.left = positionLeft + randomSpeed + 'px';
 
             //finds start line
             var finishLine = document.querySelector('#startline');
             var finish = finishLine.offsetLeft + 120;
 
             //checks horse position according to set finish position
-            if (positionLeft >= finish ){
+            if (positionLeft >= finish) {
                 horse[i].style.left = finish;
-                horse[i].style.left = positionLeft + 0 +'px'; 
+                horse[i].style.left = positionLeft + 0 + 'px';
                 horse[i].className = "horse standRight";
 
                 //adds elements to scoreboard array it is added by sequence who reached finish position first 
                 var adding = winners(scoreBoard, i);
-                
-                if(adding == false){
+
+                if (adding == false) {
                     scoreBoard.push(i)
+
+                    //stops timer and function if array length is equals to 4
+                    if (scoreBoard.length == 4) {
+
+                        clearInterval(interval);
+                        winnerTable();
+
+                    }
                 }
             }
-            //stops timer and function if array length is equals to 4
-            if(scoreBoard.length == 4){
-                winnerTable();
-                clearInterval(interval);
-            }
         }
-                    
-    }            
+
+    }
 }
 
 
 
 
 // check score board array for duplication
-function winners(array, horseNumber){    
-        var check=false;
-        for (var i = 0; i< array.length ; i++){
-            if (array[i] == horseNumber){
-               check = true;
-                
-            }
-        }
-          return check; 
-    }
+function winners(array, horseNumber) {
+    var check = false;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == horseNumber) {
+            check = true;
 
-function winnerTable(){
-    
+        }
+    }
+    return check;
+}
+// TODO
+function winnerTable() {
+
+    var element = document.getElementById("#results");
+    var winnerPosition = element.getElementsByClassName("horse");
+    for (var i = 0; i < 4; i++) {
+
+        horsepo = "horse" + (scoreBoard[i] + 1);
+        console.log(winnerPosition);
+        winnerPosition[i].className = horsepo[i];
+    }
 
 }
 
@@ -119,19 +131,19 @@ function winnerTable(){
 
 
 
-function start(){          
-        interval = setInterval(moveRight, 10);
-        
-       
-    }
-   
+function start() {
+    interval = setInterval(moveRight, 10);
 
-    
+
+}
+
+
+
 
 
 function myLoadEvent() {
     var startButton = document.querySelector('div #start');
     startButton.addEventListener('click', start);
-   }
+}
 
-   document.addEventListener('DOMContentLoaded', myLoadEvent);
+document.addEventListener('DOMContentLoaded', myLoadEvent);
